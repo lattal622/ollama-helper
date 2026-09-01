0204435b5ce0402e2f3ad238568cf393c99e898a
lattal622 <pavicgoran340@gmail.com>
2026-09-01 20:50:41 +0200

    Add postavke route with component

---
+++ src/routes/src/routes/src/routes/src/routes/postavke.tsx
@@ -0,0 +1,5 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/postavke')({
+  component: () => <div className="p-4 pt-20 text-center text-xl">Postavke aplikacije</div>,
+})
